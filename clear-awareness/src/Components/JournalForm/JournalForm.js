3a26b9493e41a6c24555cf0e99f6e12ea2c3e764
James Souksavanh Moua <Smoua1558@gmail.com>
2020-06-03 15:21:58 -0400

    add styling and implement task feature

--- clear-awareness/src/Components/JournalForm/JournalForm.js
+++ clear-awareness/src/Components/JournalForm/JournalForm.js
@@ -4,6 +4,9 @@ import './JournalForm.css';
 import TokenService from '../../Services/token-service';
 import Config from '../../config';
 import { format } from "date-fns";
+
+
+
 export default class JournalForm extends Component {
   static propTypes = {
     redirectToDashboard: PropTypes.func.isRequired,
@@ -11,7 +14,7 @@ export default class JournalForm extends Component {
   }
 
   state = {
-    
+    tasks: []
   }
   
   //will be using this function for submit handler, POST
@@ -25,7 +28,7 @@ export default class JournalForm extends Component {
       body: JSON.stringify({
         entry: e.target.textarea.value,
         mindful: e.target.mindfulAct.value,
-        tasks: e.target.tasks.value,
+        tasks: this.state.tasks,
         sleep_hours: e.target.sleep.value,
         date_created: format(this.props.date, 'yyyy-MM-dd'),
         emotions: e.target.mood.value,
@@ -34,66 +37,105 @@ export default class JournalForm extends Component {
       !res.ok ? res.json().then((err) => Promise.reject(err)) : res.json() 
     );
   }
+
+  addTask = e => {
+    e.preventDefault();
+    let task = document.getElementById("task-input").value;
+    let tasks = this.state.tasks;
+
+    if (task.length !== 0) {
+      document.getElementById("task-input").value = "";
+      tasks.push(task);
+      this.setState({tasks: tasks});
+    }
+
+  }
   
   render() {
 
     console.log('this is the date', this.props.date)
-
+    //happy, ok, fine, terrible
     return (
-     
+      <div className="journal-form">
+        <form onSubmit={this.createJournal}>
+            <fieldset className="mood">
+              <legend>Mood</legend>
+              <div className="emoji">
+                <label htmlFor="happy">
+                  <input type="radio" name="mood" id="happy" value="1" />
+                  <i className="fas fa-grin"></i>
+                </label>
+                <label htmlFor="ok">
+                  <input type="radio" name="mood" id="ok" value="2" />
+                  <i className="fas fa-smile"></i>
+                </label>
+                <label htmlFor="fine">
+                  <input type="radio" name="mood" id="fine" value="3" />
+                  <i className="fas fa-frown"></i>
+                </label>
+                <label htmlFor="terrible">
+                  <input type="radio" name="mood" id="terrible" value="4"/>
+                  <i className="fas fa-sad-tear"></i>
+                </label>
+              </div>
+            </fieldset>
 
-        <form onSubmit={this.createJournal} className="planning-block">
-          <div className="planning-block">
-            <div className="mood">
-              <label htmlFor="mood">Mood</label>
-              <label htmlFor="happy">Happy</label>
-              <input type="radio" id="mood" name="mood" value="1" />
-              <label htmlFor="ok">Ok</label>
-              <input type="radio" id="mood" name="mood" value="2" />
-              <label htmlFor="fine">Fine</label>
-              <input type="radio" id="mood" name="mood" value="3" />
-              <label htmlFor="terrible">Terrible</label>
-              <input type="radio" id="mood" name="mood" value="4" />
+            <div className="sleep-input">
+              <label htmlFor="sleep">Sleep</label>
+              <input
+                type="text"
+                id="sleep"
+                name="sleep"
+                placeholder="How much did u sleep last night?"
+              />
             </div>
 
-            <label htmlFor="sleep">Sleep</label>
-            <input
-              type="text"
-              id="sleep"
-              name="sleep"
-              placeholder="How much did u sleep last night?"
-            />
+            <div className="mindful-input">
+              <label htmlFor="mindfulAct">Mindful Act </label>
+              <input
+                type="text"
+                id="mindfulAct"
+                name="mindfulAct"
+                placeholder="Mindful Act?"
+              />
+            </div>
 
-            <label htmlFor="mindfulAct">Mindful Act </label>
-            <input
-              type="text"
-              id="mindfulAct"
-              name="mindfulAct"
-              placeholder="mindful act?"
-            />
+            <div className="task-input">
+              <label htmlFor="tasks">Tasks</label>
+              <input
+                type="text"
+                id="task-input"
+                name="tasks"
+                placeholder="Enter Tasks Here"
+                required
+              />
+              <button className="addTask" onClick={this.addTask} >Add Task</button>
+            </div>
+            
+            <div className="textarea-input">
+              <label htmlFor="textarea">Your Thoughts Here</label>
+              <textarea id="textarea" name="textarea" rows="4" col="10" placeholder="Please place thoughts here."></textarea>
+            </div>
 
-            <label htmlFor="tasks">Tasks</label>
-            <input
-              type="text"
-              id="task-input"
-              name="tasks"
-              placeholder="Enter tasks"
-            />
-            <button className="addTask">add task</button>
-
-            <label htmlFor="textarea">Your Thoughts Here</label>
-            <textarea id="textarea" name="textarea" rows="4" col="10" placeholder="Please place thoughts here."></textarea>
-            <button className="buton">submit</button>
-
+            <div className="button-input">
             <button
-              className="buton"
-              type="submit"
-              onClick={this.props.redirectToDashboard}
-            >
-              Go back
-            </button>
-          </div>
+                className="buton"
+                type="submit"
+                onClick={this.props.redirectToDashboard}
+              >
+                Go Back
+              </button>
+              <button className="buton">Submit</button>
+            </div>
         </form>
+
+        <div className="display-task">
+          {(this.state.tasks.length === 0) ? <h1></h1> : <h2>Tasks</h2>}
+          <ul className="task-list">
+            {this.state.tasks.map((task, index) => <li key={index}>{task}</li>)}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
