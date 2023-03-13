68bce85a8d87dd6b48a58e98c3f709815a020129
bpetit940 <bpetit940@gmail.com>
2023-03-13 17:30:06 -0600

    Actual server

--- clear-awareness/src/config.js
+++ clear-awareness/src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT:"https://evening-retreat-72118.herokuapp.com/",
+    API_ENDPOINT:"https://evening-retreat-72118.herokuapp.com",
     TOKEN_KEY: process.env.REACT_APP_API_KEY || "blogful-client-auth-token",
   };
   
